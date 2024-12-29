e9f4be3022196673a11344b274956176a6b88f97
AzizBenIsmail <aziz.270700@gmail.com>
2024-12-29 13:11:48 +0100

    Protection des routes

--- routes/os.js
+++ routes/os.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const authmiddlewares = require('../middlewares/authmiddlewares');
 
 const osController = require('../controllers/osController');
 /* GET home page. */
-router.get('/osInformations', osController.getOsInformation);
+router.get('/osInformations',authmiddlewares, osController.getOsInformation);
 
 module.exports = router;
